b287d0695c36d0cd2dd17c702c6822868b9c5f57
Archaengel <god11341258@gmail.com>
2019-07-25 16:40:00 -0700

    Add nodemon to work with heroku local

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const User = require('./models/Users')
 const uri = process.env.MONGODB_URI
 
 const options = {
-  "useNewUrlParser": true,
-  "dbName": 'socialUsers',
+  "useNewUrlParser": true
 }
 
 const db = mongoose.connect(uri, options)
@@ -38,5 +37,5 @@ app.post('/api/user/new', (req, res) => {
 })
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Listening on ${ process.env.PORT || 5000}...`)
+  console.log(`>>> Listening on ${ process.env.PORT || 5000}...`)
 })
